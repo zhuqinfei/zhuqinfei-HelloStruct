fc615235326c872e317222be1e8eed3df2a3da55
zhuqinfei <1586859762@qq.com>
2024-02-18 18:25:55 +0800

    结构体经典题目

--- HelloStruct/Program.cs
+++ HelloStruct/Program.cs
@@ -10,10 +10,13 @@ namespace HelloStruct
     {
         static void Main(string[] args)
         {
-            Student student = new Student() { ID = 101, Name = "Tony" };
-            object obj = student;
-            Student student2 = (Student)obj;
-            Console.WriteLine($"#{student2.ID} Name:{student2.Name}");
+            Student stu1 = new Student() { ID = 101, Name = "Tony" };
+            //结构体和类最大的区别就是，这里赋值是完全赋值过去，而不是引用，类的引用
+            Student stu2 = stu1;
+            stu2.ID = 1001;
+            stu2.Name = "Mike";
+           //打印出的内容还是之前stu1内容
+            Console.WriteLine($"#{stu1.ID} Name:{stu1.Name}");
         }
 
         //结构体使用方法和类很相似
