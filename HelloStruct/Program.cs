1a4828f1911e8e14d2418739735fe6fd9260ddaa
zhuqinfei <1586859762@qq.com>
2024-02-18 18:19:18 +0800

    c#结构体例子

---
+++ HelloStruct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student() { ID = 101, Name = "Tony" };
+            object obj = student;
+            Student student2 = (Student)obj;
+            Console.WriteLine($"#{student2.ID} Name:{student2.Name}");
+        }
+
+        //结构体使用方法和类很相似
+        struct Student
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
